32257e7ea224293b7ce5dcf02c98be7375bfd203
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-05-14 20:51:20 -0700

    Update FileCreator.py

--- FileCreator.py
+++ FileCreator.py
@@ -8,8 +8,8 @@ def get_new_seq(l):
         s += dna[t]
     return s
 
-SEQ_NO_T = 128
-SEQ_NO_Q = 128
+SEQ_NO_T = 64
+SEQ_NO_Q = 64
 length = [64, 128, 256, 512]
 length_x = [128, 256, 512]
 a = 2
@@ -19,17 +19,6 @@ gape = 2
 f = open("seqx.txt", "w")
 seqs_x = ["" for _ in range(SEQ_NO_T)]        
 l = SEQ_NO_T
-g = 0
-count_tmp = 0
-#while g < 3:
-#    count = randint(0, l)
-#    for j in range(count_tmp, count + count_tmp):
-#        seqs_x[j] = get_new_seq(length_x[g])
-#        f.write(seqs_x[j])
-#        f.write("\n")
-#    g += 1
-#    l -= count
-#    count_tmp += count
 for j in range(SEQ_NO_T):
     seqs_x[j] = get_new_seq(512)
     f.write(seqs_x[j])
@@ -38,30 +27,9 @@ f.close()
 f = open("seqy.txt", "w")
 seqs_y = ["" for _ in range(SEQ_NO_Q)]        
 l = SEQ_NO_Q
-g = 0
-count_tmp = 0
-#while g < 3:
-#    count = randint(0, l)
-#    for j in range(count_tmp, count + count_tmp):
-#        seqs_y[j] = get_new_seq(length_x[g])
-#        f.write(seqs_y[j])
-#        f.write("\n")
-#    g += 1
-#    l -= count
-#    count_tmp += count
 for j in range(SEQ_NO_Q):
     seqs_y[j] = get_new_seq(512)
     #print(seqs_y[j])
     f.write(seqs_y[j])
     f.write("\n")    
 f.close()
-f = open("seqx_seq.txt", "w")
-f2 = open("seqy_seq.txt", "w")
-for i in range(SEQ_NO_T):
-    for j in range(SEQ_NO_Q):
-        f.write(seqs_x[i])
-        f.write("\n")
-        f2.write(seqs_y[j])
-        f2.write("\n")
-f.close()
-f2.close()
