5e597bef4f8ef1c4a659087cd8592b45312f83b8
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-05-14 20:52:00 -0700

    Update FileCreator.py

--- FileCreator.py
+++ FileCreator.py
@@ -12,10 +12,7 @@ SEQ_NO_T = 64
 SEQ_NO_Q = 64
 length = [64, 128, 256, 512]
 length_x = [128, 256, 512]
-a = 2
-b = 4
-gapo = 4
-gape = 2
+
 f = open("seqx.txt", "w")
 seqs_x = ["" for _ in range(SEQ_NO_T)]        
 l = SEQ_NO_T
@@ -29,7 +26,6 @@ seqs_y = ["" for _ in range(SEQ_NO_Q)]
 l = SEQ_NO_Q
 for j in range(SEQ_NO_Q):
     seqs_y[j] = get_new_seq(512)
-    #print(seqs_y[j])
     f.write(seqs_y[j])
     f.write("\n")    
 f.close()
