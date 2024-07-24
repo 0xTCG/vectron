8a25e97a1c0fc61828994e6cc1701b7b457069ad
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-07-24 16:24:48 -0700

    Update seq_modifier_16.py

--- experiments_docker/seq_modifier_16.py
+++ experiments_docker/seq_modifier_16.py
@@ -1,18 +1,18 @@
 var = 16
 
 # Read sequences from seqx.txt and seqy.txt
-with open("./data/FLOAT_small/seqx.txt", "r") as file:
+with open("/vectron/experiments_docker/data/FLOAT_small/seqx.txt", "r") as file:
     seqx_data = file.readlines()
 
-with open("./data/FLOAT_small/seqy.txt", "r") as file:
+with open("/vectron/experiments_docker/data/FLOAT_small/seqy.txt", "r") as file:
     seqy_data = file.readlines()
 
-with open("./data/CUDA_small/seqx_256.txt", "w") as file:
+with open("/vectron/experiments_docker/data/CUDA_small/seqx_256.txt", "w") as file:
     for j in range(len(seqx_data)):
         for i in range(var):
             file.writelines(seqx_data[j])
 
-with open("./data/CUDA_small/seqy_256.txt", "w") as file:
+with open("/vectron/experiments_docker/data/CUDA_small/seqy_256.txt", "w") as file:
     for i in range(var):
         file.writelines(seqy_data)
 
