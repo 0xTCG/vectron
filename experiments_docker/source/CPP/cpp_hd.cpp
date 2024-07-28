0b978ead58191a9ced695358a0464d26dd996b0c
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-07-28 12:09:16 -0700

    Update cpp_hd.cpp

--- experiments_docker/source/CPP/cpp_hd.cpp
+++ experiments_docker/source/CPP/cpp_hd.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -43,7 +44,7 @@ void align(std::vector<int16_t> &scores, std::vector<dp_mat> &matrices,
     }
 }
 
-void sw_cpu(std::vector<std::pair<std::string, std::string>> const &sequences) {
+void sw_cpu(std::vector<std::pair<std::string, std::string>> const &sequences, int QUANTITY) {
     std::vector<int16_t> scores(QUANTITY);
     std::vector<dp_mat> matrices(QUANTITY, dp_mat(SIZE + 1, std::vector<int16_t>(SIZE + 1)));
 
@@ -93,7 +94,7 @@ int main(int argc, char* argv[]) {
 
     std::vector<std::pair<std::string, std::string>> paired_sequences = pair_sequences(target_sequences, query_sequences);
 
-    sw_cpu(paired_sequences);
+    sw_cpu(paired_sequences, QUANTITY);
 
     return 0;
 }
