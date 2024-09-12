6ca9b54d5bcc47719e18396158ab8e542484da0b
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-09-12 15:13:34 -0700

    Corrections to avoid jupyter errors

--- docker/experiments_docker/source/cpp/lcs.cpp
+++ docker/experiments_docker/source/cpp/lcs.cpp
@@ -8,56 +8,51 @@
 
 constexpr int SIZE = 512;
 int QUANTITY = 0;
-constexpr int BATCH_SIZE = 262144;
  
 using dp_mat = std::vector<std::vector<int16_t>>;
 
 void align(std::vector<int16_t> &scores, std::vector<dp_mat> &matrices,
-           const std::vector<std::pair<std::string, std::string>> &sequences, int start, int end) {
+           std::vector<std::pair<std::string, std::string>> const &sequences, int QUANTITY) {
     auto const gap_o = 0;
     auto const mismatch = 0;
     auto const match = 1;
     auto const gap_e = 1;
     auto const ambig = 0;
 
-    for (int t = start; t < end; ++t) {
-        int16_t target_value = 0;
+    for (int t = 0; t < QUANTITY; ++t) {
+        int16_t target_value;
         int16_t max_value = 0;
         for (int16_t i = 1; i < SIZE; ++i) {
             for (int16_t j = 1; j < SIZE; ++j) {
-                int16_t diagonal_value = matrices[t - start][i - 1][j - 1];
+
+                int16_t diagonal_value = matrices[t][i - 1][j - 1];
                 if (sequences[t].first[i - 1] == 'N' || sequences[t].second[j - 1] == 'N') {
                     diagonal_value += ambig;
                 } else {
                     diagonal_value += (sequences[t].first[i - 1] == sequences[t].second[j - 1] ? match : mismatch);
                 }
-
-                int16_t top_value = matrices[t - start][i - 1][j];
-                int16_t left_value = matrices[t - start][i][j - 1];
+                int16_t top_value = matrices[t][i - 1][j];
+                int16_t left_value = matrices[t][i][j - 1];
                 int16_t temp = top_value - ((top_value - left_value) & ((top_value - left_value) >> (sizeof(int16_t) * 8 - 1)));
                 target_value = diagonal_value - ((diagonal_value - temp) & ((diagonal_value - temp) >> (sizeof(int16_t) * 8 - 1)));
-                matrices[t - start][i][j] = target_value;
+                matrices[t][i][j] = target_value;
+
+
             }
         }
-        scores[t] = target_value;
+        scores[t] = target_value;//max_value - target_value > 800 ? target_value : -32768;
     }
 }
 
-void sw_cpu(const std::vector<std::pair<std::string, std::string>> &sequences, int QUANTITY) {
+void sw_cpu(std::vector<std::pair<std::string, std::string>> const &sequences, int QUANTITY) {
     std::vector<int16_t> scores(QUANTITY);
+    std::vector<dp_mat> matrices(QUANTITY, dp_mat(SIZE, std::vector<int16_t>(SIZE)));
 
     auto const start_time = std::chrono::steady_clock::now();
-
-    for (int start = 0; start < QUANTITY; start += BATCH_SIZE) {
-        int end = std::min(start + BATCH_SIZE, QUANTITY);
-        std::vector<dp_mat> matrices(end - start, dp_mat(SIZE, std::vector<int16_t>(SIZE)));
-        align(scores, matrices, sequences, start, end);
-    }
-
+    align(scores, matrices, sequences, QUANTITY);
     for (auto e : scores) {
         std::cout << e << "\n";
     }
-
     auto const end_time = std::chrono::steady_clock::now();
     std::cout << std::fixed << std::setprecision(2)
               << std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count() / 1e6
