8dd2063ebe87bcb1286e248edf407b7203cca82f
Sourenm <114013338+Sourenm@users.noreply.github.com>
2024-09-12 21:11:40 -0700

    Update needleman_wunsch.cpp

--- docker/experiments_docker/source/cpp/needleman_wunsch.cpp
+++ docker/experiments_docker/source/cpp/needleman_wunsch.cpp
@@ -11,8 +11,8 @@ int QUANTITY = 0;
  
 using dp_mat = std::vector<std::vector<int16_t>>;
 
-void align(std::vector<int16_t> &scores, std::vector<dp_mat> &matrices,
-           std::vector<dp_mat> &matrices_left, std::vector<dp_mat> &matrices_top,
+void align(std::vector<int16_t> &scores, dp_mat &matrix,
+           dp_mat &matrix_left, dp_mat &matrix_top,
            std::vector<std::pair<std::string, std::string>> const &sequences, int QUANTITY) {
     auto const gap_o = -4;
     auto const mismatch = -4;
@@ -21,35 +21,35 @@ void align(std::vector<int16_t> &scores, std::vector<dp_mat> &matrices,
     auto const ambig = -3;
 
     for (int t = 0; t < QUANTITY; ++t) {
-        matrices[t][0][0] = 0;
-        matrices_left[t][0][0] = -10000;
-        matrices_top[t][0][0] = -10000;
+        matrix[0][0] = 0;
+        matrix_left[0][0] = -10000;
+        matrix_top[0][0] = -10000;
         for (int16_t i = 1; i < SIZE + 1; ++i) {
-            matrices[t][0][i] = gap_o + gap_e * i;
-            matrices[t][i][0] = gap_o + gap_e * i;
-            matrices_left[t][0][i] = gap_o + gap_e * i;
-            matrices_left[t][i][0] = -10000;
-            matrices_top[t][0][i] = -10000;
-            matrices_top[t][i][0] = gap_o + gap_e * i;
+            matrix[0][i] = gap_o + gap_e * i;
+            matrix[i][0] = gap_o + gap_e * i;
+            matrix_left[0][i] = gap_o + gap_e * i;
+            matrix_left[i][0] = -10000;
+            matrix_top[0][i] = -10000;
+            matrix_top[i][0] = gap_o + gap_e * i;
         }
         int16_t target_value;
         int16_t max_value = 0;
         for (int16_t i = 1; i < SIZE + 1; ++i) {
             for (int16_t j = 1; j < SIZE + 1; ++j) {
 
-                int16_t diagonal_value = matrices[t][i - 1][j - 1];
+                int16_t diagonal_value = matrix[i - 1][j - 1];
                 if (sequences[t].first[i - 1] == 'N' || sequences[t].second[j - 1] == 'N') {
                     diagonal_value += ambig;
                 } else {
                     diagonal_value += (sequences[t].first[i - 1] == sequences[t].second[j - 1] ? match : mismatch);
                 }
-                int16_t top_value = matrices[t][i - 1][j] - 5;
-                int16_t left_value = matrices[t][i][j - 1] - 5;
+                int16_t top_value = matrix[i - 1][j] - 5;
+                int16_t left_value = matrix[i][j - 1] - 5;
                 int16_t temp = top_value - ((top_value - left_value) & ((top_value - left_value) >> (sizeof(int16_t) * 8 - 1)));
                 target_value = diagonal_value - ((diagonal_value - temp) & ((diagonal_value - temp) >> (sizeof(int16_t) * 8 - 1)));
-                matrices[t][i][j] = target_value;
-                matrices_left[t][i][j] = left_value;
-                matrices_top[t][i][j] = top_value;
+                matrix[i][j] = target_value;
+                matrix_left[i][j] = left_value;
+                matrix_top[i][j] = top_value;
                 if (target_value > max_value)
                     max_value = target_value;
                 
@@ -61,12 +61,12 @@ void align(std::vector<int16_t> &scores, std::vector<dp_mat> &matrices,
 
 void sw_cpu(std::vector<std::pair<std::string, std::string>> const &sequences, int QUANTITY) {
     std::vector<int16_t> scores(QUANTITY);
-    std::vector<dp_mat> matrices(QUANTITY, dp_mat(SIZE + 1, std::vector<int16_t>(SIZE + 1)));
-    std::vector<dp_mat> matrices_left(QUANTITY, dp_mat(SIZE + 1, std::vector<int16_t>(SIZE + 1)));
-    std::vector<dp_mat> matrices_top(QUANTITY, dp_mat(SIZE + 1, std::vector<int16_t>(SIZE + 1)));
+    dp_mat matrix(SIZE + 1, std::vector<int16_t>(SIZE + 1));
+    dp_mat matrix_left(SIZE + 1, std::vector<int16_t>(SIZE + 1));
+    dp_mat matrix_top(SIZE + 1, std::vector<int16_t>(SIZE + 1));
 
     auto const start_time = std::chrono::steady_clock::now();
-    align(scores, matrices, matrices_left, matrices_top, sequences, QUANTITY);
+    align(scores, matrix, matrix_left, matrix_top, sequences, QUANTITY);
     for (auto e : scores) {
         std::cout << e << "\n";
     }
